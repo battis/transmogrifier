b5338ad24b7fd1b532fddd6fb7305e7932e4d07e
Seth Battis <seth@battis.net>
2023-02-13 18:10:04 +0000

    Refactored

--- public/index.php
+++ public/index.php
@@ -1,7 +1,11 @@
 <?php
 
-$data = '';
+$php =['serialize','unserialize','urlencode','urldecode','base64_encode','base64_decode','strtotime','hash','password_hash'];
+$submenus = ['user','password_hash','hash'];
+
+$data = $_REQUEST['data'];
 $error = false;
+
 if (isset($_REQUEST['transmogrification'])) {
     $transmogrification = $_REQUEST['transmogrification'];
 	switch($transmogrification) {
@@ -19,10 +23,9 @@ if (isset($_REQUEST['transmogrification'])) {
             // intentional fall-through
         default:
             try {
-            $data = call_user_func($transmogrification, $_REQUEST['data']);
-            } catch (Error $e) {
-                $error = true;
-                $data = $_REQUEST['data'];
+                $data = call_user_func($transmogrification, $_REQUEST['data']);
+            } catch (Throwable $e) {
+                $error = str_replace('call_user_func(): Argument #1 ($callback) must be a valid callback, ','', $e->getMessage());
             }
 	}
 }
@@ -39,11 +42,8 @@ EOT;
     return join(PHP_EOL, $options);
 }
 
-$php =['serialize','unserialize','urlencode','urldecode','base64_encode','base64_decode','strtotime','hash','password_hash'];
-$submenus = ['user','password_hash','hash'];
-
 ?>
-<!DOCTYPE html lang="en">
+<!doctype html lang="en">
 <html>
 	<head>
     <meta charset="utf-8">
@@ -51,36 +51,43 @@ $submenus = ['user','password_hash','hash'];
 		<title>Transmogrifier</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <style>
-            textarea {
+            .data-entry {
                 font-family: monospace;
             }
+            .btn.transmogrify img {
+                height: 1em;
+            }
+            .hidden {
+                display: none;
+            }
         </style>
-x  	</head>
-	<body onload="document.getElementById('data').select();">
+  	</head>
+	<body>
         <div class="container">
-		<form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
+            <div class="alert alert-danger<?= $error ? '' : ' hidden'?>" role="alert"><?= $error ?></div>
+		    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
                 <div class="input-group">
-                <select name="transmogrification" id="transmogrification" class="form-control">
-                    <optgroup label="PHP">
-                    <?= optionsFromArray($php, 'transmogrification', 'urlencode') ?>
-                    </optgroup>
-                    <optgroup>
-                    <option value="user"<?= !empty($_REQUEST['transmogrification']) && $_REQUEST['transmogrification'] == 'user' ? ' selected' : '' ?>>Manually entered</option>
-                    </optgroup>
-                </select>
-                <input type="text" name="user" id="user" value="<?= $_REQUEST['user'] ?? ''?>" class="form-control"/>
-                <select name="hash" class="form-control<?= $error ? ' error' : '' ?>" id="hash">
-					<?= optionsFromArray(hash_algos(), 'hash', 'md5')?>
-				</select>
-                <select name="password_hash" class="form-control<?= $error ? ' error' : '' ?>" id="password_hash">
-                    <?= optionsFromArray(password_algos(), 'password_hash', PASSWORD_DEFAULT)?>
-                </select>
-                <button type="submit" class="btn btn-primary">Transmogrify</button>
+                    <select name="transmogrification" id="transmogrification" class="form-control">
+                        <optgroup label="PHP">
+                        <?= optionsFromArray($php, 'transmogrification', 'urlencode') ?>
+                        </optgroup>
+                        <optgroup>
+                        <option value="user"<?= !empty($_REQUEST['transmogrification']) && $_REQUEST['transmogrification'] == 'user' ? ' selected' : '' ?>>Manually entered</option>
+                        </optgroup>
+                    </select>
+                    <input type="text" name="user" id="user" value="<?= $_REQUEST['user'] ?? ''?>" class="form-control"/>
+                    <select name="hash" class="form-control<?= $error ? ' error' : '' ?>" id="hash">
+					    <?= optionsFromArray(hash_algos(), 'hash', 'md5')?>
+				    </select>
+                    <select name="password_hash" class="form-control" id="password_hash">
+                        <?= optionsFromArray(function_exists('password_algos') ? password_algos() : [PASSWORD_DEFAULT], 'password_hash', PASSWORD_DEFAULT)?>
+                    </select>
+                    <button type="submit" class="btn btn-primary transmogrify"><img src="transmogrifier.png" /></button>
                 </div>
-			<div id="data-entry">
-				<textarea class="form-control" cols="80" rows="<?= max(20, substr_count($data, "\n") + 3) ?>" id="data" name="data"><?= $data ?></textarea>
-			</div>
-		</form>
+			    <div id="data-entry">
+				    <textarea class="form-control" cols="80" rows="<?= max(20, substr_count($data, "\n") + 3) ?>" id="data" name="data"><?= $data ?></textarea>
+			    </div>
+		    </form>
         </div>
         <script>
             const t = document.getElementById('transmogrification');
@@ -106,6 +113,8 @@ x  	</head>
             }, {passive: true});
 
             updateHashVisibility(t);
+
+            document.getElementById('data').select();
         </script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>

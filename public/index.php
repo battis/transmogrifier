bc0d80340b08a677967acb77db7298d4765b95cd
Seth Battis <seth@battis.net>
2023-02-13 16:11:12 +0000

    Cleaning

--- public/index.php
+++ public/index.php
@@ -5,32 +5,42 @@ $error = false;
 if (isset($_REQUEST['transmogrification'])) {
     $transmogrification = $_REQUEST['transmogrification'];
 	switch($transmogrification) {
-		case 'unserialize': {
+		case 'unserialize':
 			$data = print_r(unserialize($_REQUEST['data']), true);
 			break;
-		}
-		case 'hash': {
+		case 'hash':
 			$data = hash($_REQUEST['hash'], $_REQUEST['data']);
 			break;
-		}
-		case 'password_hash': {
-			$data = password_hash($_REQUEST['data'], PASSWORD_DEFAULT);
+		case 'password_hash':
+			$data = password_hash($_REQUEST['data'], $_REQUEST['password_hash']);
 			break;
-		}
         case 'user':
             $transmogrification = $_REQUEST['user'];
-        default: {
+            // intentional fall-through
+        default:
             try {
             $data = call_user_func($transmogrification, $_REQUEST['data']);
-        } catch (Error $e) {
-            $error = true;
-            $data = $_REQUEST['data'];
-        }
-        }
+            } catch (Error $e) {
+                $error = true;
+                $data = $_REQUEST['data'];
+            }
 	}
 }
 
-$php =['serialize','unserialize','urlencode','urldecode','base64_encode','base64_decode','strtotime','hash','password_hash']
+function optionsFromArray($arr, $key, $default) {
+    $options = [];
+    $selector = $_REQUEST[$key] ?? $default;
+    foreach($arr as $opt) {
+        $selected = $opt == $selector ? ' selected' : '';
+        $options[] = <<<EOT
+<option value="{$opt}"{$selected}>{$opt}</option>
+EOT;
+    }
+    return join(PHP_EOL, $options);
+}
+
+$php =['serialize','unserialize','urlencode','urldecode','base64_encode','base64_decode','strtotime','hash','password_hash'];
+$submenus = ['user','password_hash','hash'];
 
 ?>
 <!DOCTYPE html lang="en">
@@ -39,24 +49,20 @@ $php =['serialize','unserialize','urlencode','urldecode','base64_encode','base64
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Transmogrifier</title>
-		<style>
-			textarea {
-				font-family: monospace;
-			}
-		</style>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-	</head>
+        <style>
+            textarea {
+                font-family: monospace;
+            }
+        </style>
+x  	</head>
 	<body onload="document.getElementById('data').select();">
         <div class="container">
 		<form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
                 <div class="input-group">
                 <select name="transmogrification" id="transmogrification" class="form-control">
                     <optgroup label="PHP">
-                    <?php
-                    foreach($php as $function) {
-                        echo "<option value=\"$function\"".($function == $_REQUEST['transmogrification'] ? ' selected':'').">$function</option>";
-                    }
-                    ?>
+                    <?= optionsFromArray($php, 'transmogrification', 'urlencode') ?>
                     </optgroup>
                     <optgroup>
                     <option value="user"<?= !empty($_REQUEST['transmogrification']) && $_REQUEST['transmogrification'] == 'user' ? ' selected' : '' ?>>Manually entered</option>
@@ -64,12 +70,11 @@ $php =['serialize','unserialize','urlencode','urldecode','base64_encode','base64
                 </select>
                 <input type="text" name="user" id="user" value="<?= $_REQUEST['user'] ?? ''?>" class="form-control"/>
                 <select name="hash" class="form-control<?= $error ? ' error' : '' ?>" id="hash">
-					<?php
-						foreach (hash_algos() as $hash) {
-							echo "<option value=\"$hash\"" . ($hash == $_REQUEST['hash']? ' selected' : '') . ">$hash</option>";
-						}
-					?>
+					<?= optionsFromArray(hash_algos(), 'hash', 'md5')?>
 				</select>
+                <select name="password_hash" class="form-control<?= $error ? ' error' : '' ?>" id="password_hash">
+                    <?= optionsFromArray(password_algos(), 'password_hash', PASSWORD_DEFAULT)?>
+                </select>
                 <button type="submit" class="btn btn-primary">Transmogrify</button>
                 </div>
 			<div id="data-entry">
@@ -79,22 +84,23 @@ $php =['serialize','unserialize','urlencode','urldecode','base64_encode','base64
         </div>
         <script>
             const t = document.getElementById('transmogrification');
-            const algorithm = document.getElementById('hash');
-            const user = document.getElementById('user');
+            <?php foreach ($submenus as $submenu) { echo <<< EOT
+            const {$submenu} = document.getElementById('{$submenu}');
+            EOT; } ?>
             function updateHashVisibility(transmogrification) {
-                if (transmogrification.value == 'hash') {
-                    algorithm.style.display = 'inline-block';
+            <?php foreach($submenus as $submenu) { echo <<< EOT
+                if (transmogrification.value == '{$submenu}') {
+                    {$submenu}.style.display = 'inline-block';
                 } else {
-                    algorithm.style.display = 'none';
+                    {$submenu}.style.display = 'none';
                 }
-
-                 if (transmogrification.value == 'user') {
-                user.style.display = 'inline-block';
-            } else {
-                user.style.display = 'none';
+            EOT; } ?>
+                if (transmogrification.value == 'user') {
+                    user.style.display = 'inline-block';
+                } else {
+                    user.style.display = 'none';
+                }
             }
-
-}
             t.addEventListener('change', (event) => {
                 updateHashVisibility(event.target)
             }, {passive: true});
